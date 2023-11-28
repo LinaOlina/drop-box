a3de8b77a4321aac7cb838f9f1cc6326dc7da5be
liina <liina.oliin@gmail.com>
2023-11-28 08:14:12 +0100

    started with authentication, added security dependencies and security config with some code

---
+++ src/main/java/com/lina/individuelluppgift/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.lina.individuelluppgift.config;
+
+import com.lina.individuelluppgift.user.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity security) throws  Exception {
+        security.authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll());
+        return security.build();
+    }
+}
--- src/main/java/com/lina/individuelluppgift/user/User.java
+++ src/main/java/com/lina/individuelluppgift/user/User.java
@@ -2,13 +2,16 @@ package com.lina.individuelluppgift.user;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.UUID;
 
 @Entity
 @Data
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -17,4 +20,28 @@ public class User {
     private String password;
     private String email;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
